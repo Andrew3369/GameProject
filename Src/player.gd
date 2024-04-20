4a70bc621fc84fe5fedf0cb5aa3bb068af6f3e8c
Andrew3369 <andrewjanosbabos@gmail.com>
2024-04-20 18:05:55 -0400

    Update(s)
    - Added more to the starting area
    - Optimized if statements into a match statement (switch statement). Instead of checking through if's in all 4 directions, i created a match statement that looks for which direction the player is moving. Then applies the correct animation to that direction

--- Src/player.gd
+++ Src/player.gd
@@ -4,14 +4,11 @@ extends CharacterBody2D
 
 @onready var anim = get_node("AnimatedSprite2D")
 
+
 # Base player stats
 var health = null
 const speed = 75.0
 var dmg = null
-
-#func _init(arg):
-	#health = 100
-	#dmg = 5
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,57 +16,29 @@ func _ready():
 	anim.play("idle")
 	pass # Replace with function body.
 	
-#func betterMovement():
-	#var directionX = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	#var directionY = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	#
-	#match directionX && direcitonY:
-		#(1, 0):
-			#move_right()
-		#_:
-			#if get_node("AnimatedSprite2D").animation != "attackX":
-				#anim.play("idle")
-			#velocity.x = move_toward(velocity.x, 0, speed)
-			#velocity.y = move_toward(velocity.y, 0, speed)
-		
-		
-func move_right():
-	get_node("AnimatedSprite2D").flip_h = false
-	anim.play("right")
-	velocity.x = speed
-	
+
 func PlayerMovement():
-	var directionX = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var directionY = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
+	
+	match input_direction:
+		Vector2(1, 0):  # move_right
+			get_node("AnimatedSprite2D").flip_h = false
+			anim.play("right")
 
-	#Determine which animation to play based on direction
-	if directionX < 0:
-		get_node("AnimatedSprite2D").flip_h = true
-		anim.play("right")
-		
-	elif directionX > 0:
-		get_node("AnimatedSprite2D").flip_h = false
-		anim.play("right")
+		Vector2(-1, 0):  # move_left
+			get_node("AnimatedSprite2D").flip_h = true
+			anim.play("right")
 
-	elif directionY == -1: # Run up
-		anim.play("up")
+		Vector2(0, 1):  # move_up
+			anim.play("down")
 
-	elif directionY == 1:
-		anim.play("down") # Run down
+		Vector2(0, -1):  # move_down
+			anim.play("up")
 
-	if directionX != 0 or directionY != 0:
-		velocity.x = directionX * speed
-		velocity.y = directionY * speed
-
-	else:
-		if get_node("AnimatedSprite2D").animation != "attackX":
+		Vector2():  # standing still
 			anim.play("idle")
-			
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.y = move_toward(velocity.y, 0, speed)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	PlayerMovement()
 	move_and_slide()
