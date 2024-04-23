466d420f66c58898c0f25eb23de7fa20de1d8b62
Andrew3369 <andrewjanosbabos@gmail.com>
2024-04-22 23:22:45 -0400

    Attack and Improvements
    - Attempted at adding attack animation
    - Added more to the map
    - Started a NPC node as an 'Enemy'

--- Src/player.gd
+++ Src/player.gd
@@ -44,31 +44,39 @@ class Player extends CharacterBody2D:
 # Tis here until OOP starts working
 @onready var anim = get_node("AnimatedSprite2D")
 const SPEED = 75.0
+
 func PlayerMovement():
-		var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-		velocity = input_direction * SPEED
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * SPEED
+	
+	if Input.is_action_pressed("attack"):
+		anim.play("attack")
 
-		match input_direction:
-			Vector2(1, 0):  # move_right
-				get_node("AnimatedSprite2D").flip_h = false
-				anim.play("right")
+	match input_direction:
+		Vector2(1, 0):  # move_right
+			get_node("AnimatedSprite2D").flip_h = false
+			anim.play("right")
 
-			Vector2(-1, 0):  # x coor, move left
-				get_node("AnimatedSprite2D").flip_h = true
-				anim.play("right")
+		Vector2(-1, 0):  # move_left
+			get_node("AnimatedSprite2D").flip_h = true
+			anim.play("right")
 
-			Vector2(0, 1):  # y coor, move down
-				anim.play("down")
+		Vector2(0, 1):  # move_down
+			anim.play("down")
 
-			Vector2(0, -1):  # y coor, move up
-				anim.play("up")
+		Vector2(0, -1):  # move_up
+			anim.play("up")
 
-			Vector2(0, 0):  # idle, no input
-				anim.play("idle")
+		Vector2(0, 0):  # idle
+			anim.play("idle")
 
 func _ready():
 	anim.play("idle")
 
 func _process(delta):
-	PlayerMovement() # Movement
+	PlayerMovement()  # Movement
 	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished():
+	anim.play("idle")
